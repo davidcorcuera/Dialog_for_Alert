29a000a144ebb4f0344710df09e7f67b51df4cd3
David Corcuera <david.corcuera@gmail.com>
2020-11-14 19:57:27 +0100

    Actividad 2.7: Dialog Alert Task 4: Use a dialog to request a user's choice

---
+++ app/src/main/java/es/jesuitas/dam/dialogforalert/MainActivity.java
@@ -0,0 +1,46 @@
+package es.jesuitas.dam.dialogforalert;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new
+                AlertDialog.Builder(MainActivity.this);
+        // Set the dialog title and message.
+        myAlertBuilder.setTitle(R.string.alert_title);
+        myAlertBuilder.setMessage(R.string.alert_message);
+
+        myAlertBuilder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // User clicked OK button.
+                Toast.makeText(getApplicationContext(), R.string.pressed_ok,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myAlertBuilder.setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // User clicked Cancel button.
+                Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Create and show the AlertDialog.
+        myAlertBuilder.show();
+    }
+}
